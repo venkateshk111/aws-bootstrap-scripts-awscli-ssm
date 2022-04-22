904551bc02c3ef07616b1fc0df5bc8e62cc6c8d9
Venkatesh K <47455183+venkateshk111@users.noreply.github.com>
2022-04-22 17:31:00 +0530

    Update ssm-awscli-install-amazon-rhel-centos.sh

--- ssm-awscli-install-amazon-rhel-centos.sh
+++ ssm-awscli-install-amazon-rhel-centos.sh
@@ -4,7 +4,7 @@ cd /tmp
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 sudo systemctl enable amazon-ssm-agent
 sudo systemctl start amazon-ssm-agent
-yum install unzip -y
+sudo yum install unzip -y
 #aws cli v2
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
